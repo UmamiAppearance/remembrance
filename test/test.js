09f8b7a5c47850f84fae7cc4a28827e03fcc689b
UmamiAppearance <mail@umamiappearance.eu>
2023-05-15 23:10:02 +0200

    v0.1.0 test file

--- test/test.js
+++ test/test.js
@@ -13,16 +13,22 @@ const forkPromise = (modulePath, args, options) => {
 const resultSymbol = [ "✔", "✖" ];
 
 const testRun = async (info, cwd, expectError=false) => {
+    // run test
     let result = await forkPromise("../../../index.js", [], { cwd });
     
     // flip result if error is expected
     if (expectError) result ^= 1; 
     
-    process.stdout.write(`    ${resultSymbol[result]} ${info}\n`);
+    // log result
+    console.log(`    ${resultSymbol[result]} ${info}`);
 };
 
-await testRun("Single file build success", "./test/fixtures/projectA");
-await testRun("Single file build fail", "./test/fixtures/projectB", true);
-await testRun("Multi file build success", "./test/fixtures/projectC");
-await testRun("Multi file build fail", "./test/fixtures/projectD", true);
-await testRun("File extension modification", "./test/fixtures/projectE");
+
+// test runs
+await testRun("Single file build success.", "./test/fixtures/projectA");
+await testRun("Single file build fail.", "./test/fixtures/projectB", true);
+await testRun("Multi file build success.", "./test/fixtures/projectC");
+await testRun("Multi file build fail.", "./test/fixtures/projectD", true);
+await testRun("File extension modification.", "./test/fixtures/projectE");
+
+console.log("");
