1874087586f5a06f27f57a77544016cb96ac5855
UmamiAppearance <mail@umamiappearance.eu>
2023-07-05 21:31:45 +0200

    Bump eslint from 8.41.0 to 8.44.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
  * [remembrance]{@link https://github.com/UmamiAppearance/remembrance}
  *
- * @version 0.3.0
+ * @version 0.3.1
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license MIT
  */
